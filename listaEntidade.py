83efd95a0569a3c48a7f9eaf7b1b9dc009758a4b
danidosreis <39527066+danidosreis@users.noreply.github.com>
2020-06-02 14:30:14 -0300

    Update listaEntidade.py

--- listaEntidade.py
+++ listaEntidade.py
@@ -1,4 +1,4 @@
-group_entidade = [('Cliente-Agility', '1'), ('Cliente-XXXXX', '69')]
+group_entidade = [('Cliente-XXXXXX', '1'), ('Cliente-XXXXX', '69')]
 
 def listaEntidade():
     return group_entidade
